aaaf55a548ee141d62f4bb2954635827003465c4
Adegbemi Adeolu <adegbemiadeolu@gmail.com>
2021-08-22 20:05:43 +0100

    updates

--- src/systems/Home.js
+++ src/systems/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from '../components/form/FormElements'
 
 export default function Home() {
     return (
         <div>
             <h1> I am Home</h1>
+            <Link to="/signup">Sign Up Here</Link>
         </div>
     )
 }
--- src/systems/Page.js
+++ src/systems/Page.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Page() {
     return (
         <div>
-            <h1>Thank You</h1>
+            <h1>Access content subscribed to.</h1>
+            <p>This appears only on signup and after google authentication. This is just a simple demonstration of conditional redirect</p>
+            
         </div>
     )
 }
